7465693ed8eada6c5a2d868eb276718e386655c7
Jonathan Heriyanto <jonathan.heriyanto@gmail.com>
2023-03-25 10:55:56 +0700

    Initial Commit

---
+++ GuessMojiPlsHappen/GuessMojiPlsHappenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessMojiPlsHappenApp.swift
+//  GuessMojiPlsHappen
+//
+//  Created by Jonathan Heriyanto on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessMojiPlsHappenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
